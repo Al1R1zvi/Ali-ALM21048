d8d550cce3586660f19a31c7a6afba4376741041
stuwe <stuwe@DESKTOP-J3AN2N9>
2021-07-12 20:29:06 +0530

    Day 6

---
+++ SM Week 2 Lab/src/PrintInt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class PrintInt {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter an integer... ");
+		int integer = scan.nextInt();
+		
+		System.out.println("You entered the number  "+integer);
+		
+
+	}
+
+}
